9f0327c5a9f8873166fc5853d16a6934402700db
Jian Wu <xcb479@alumni.ku.dk>
2019-02-17 20:39:03 +0530

    start cpp

--- example.cpp
+++ example.cpp
@@ -1,18 +1,11 @@
-// #include <GLXW/glxw.h>
-// #include <GLFW/glfw3.h>
 #include <iostream>
+#include <stdio.h>
+
+using namespace std;
 
 int main() {
-    int width = 640;
-    int height = 480;
+    cout << "Start running..." << endl;
+    cout << "Do something here..." << endl;
 
-    std::cout << "Start running..." << std::endl;
-
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    // glfwCreateWindow(640, 480, "Hello World", NULL, NULL );
-    
     return 0;
 }
